9feb3bbb7487b3d40d928e144a5e923c71409050
Thauan <thauansilva243@gmail.com>
2025-02-17 21:43:52 -0400

    feat: add section parceiros

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -338,24 +338,75 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div>
-          <div>
-            <h3>CONHEÇA OS NOSSOS</h3>
-            <h1>Parceiros</h1>
+        <div className="flex flex-col items-center gap-y-10 p-12">
+          <div className="text-center">
+            <h3 className="text-base font-bold text-secondaryColor py-4">
+              CONHEÇA OS NOSSOS
+            </h3>
+            <h1 className="font-semibold text-5xl">Parceiros</h1>
+          </div>
+          <div className="flex gap-x-8">
+            <div className="bg-gray-100 rounded-lg w-[280px] h-[164px] flex items-center justify-center">
+              <div className="w-[200px] h-[100px] flex items-center justify-center">
+                <a href="">
+                  <img
+                    src="https://quickdigitalbank.com.br/wp-content/uploads/2024/07/logo-parceiros.png"
+                    alt=""
+                  />
+                </a>
+              </div>
+            </div>
+            <div className="bg-gray-100 rounded-lg w-[280px] h-[164px] flex items-center justify-center">
+              <div className="w-[200px] h-[100px] flex items-center justify-center">
+                <a href="">
+                  <img
+                    src="https://quickdigitalbank.com.br/wp-content/uploads/2024/08/tecban.png"
+                    alt=""
+                  />
+                </a>
+              </div>
+            </div>
+            <div className="bg-gray-100 rounded-lg w-[280px] h-[164px] flex items-center justify-center">
+              <div className="w-[200px] h-[100px] flex items-center justify-center">
+                <a href="">
+                  <img
+                    src="https://quickdigitalbank.com.br/wp-content/uploads/2024/08/atmosfera.png"
+                    alt=""
+                  />
+                </a>
+              </div>
+            </div>
+            <div className="bg-gray-100 rounded-lg w-[280px] h-[164px] flex items-center justify-center">
+              <div className="w-[200px] h-[100px] flex items-center justify-center">
+                <a href="">
+                  <img
+                    src="https://quickdigitalbank.com.br/wp-content/uploads/2024/08/founder-institute.png"
+                    alt=""
+                  />
+                </a>
+              </div>
+            </div>
           </div>
           <div>
-            <div>
-              <a href="">
-                <img
-                  src="https://quickdigitalbank.com.br/wp-content/uploads/2024/07/logo-parceiros.png"
-                  alt=""
-                />
-              </a>
-            </div>
-            <a href=""></a>
-            <a href=""></a>
+            <Button
+              variant="default"
+              className="gap-2 py-3 px-5 bg-primaryColor hover:bg-[#9D321E] rounded-lg h-auto font-medium text-lg"
+            >
+              Quero ser um parceiro
+            </Button>
           </div>
         </div>
+        {/* <section className="flex flex-row items-center justify-center px-28">
+          <div className="w-[45%] flex flex-col p-5">
+            <h3 className="text-secondaryColor font-bold text-base">
+              AINDA NÃO ESTÁ CONVENCIDO?
+            </h3>
+            <h1 className="text-5xl font-semibold">
+              A Quick Digital é a solução para o seu negócio!
+            </h1>
+          </div>
+          <div className="flex-grow p-5"></div>
+        </section> */}
       </div>
     </>
   );
